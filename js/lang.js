7f780b4eb654d1386521c1f6a0051ee5ad476f64
Anthony Dillon <anthony.dillon@canonical.com>
2011-12-20 16:30:40 +0000

    All text is now in HTML format for translation

---
+++ js/lang.js
@@ -0,0 +1,113 @@
+var hiddenTranslation = $('#Translation-Strings');
+
+/*
+ * Email system
+ */
+
+var _email_1_title_ = hiddenTranslation.find('._email_1_title_').text();
+var _email_2_title_ = hiddenTranslation.find('._email_2_title_').text();
+var _email_3_title_ = hiddenTranslation.find('._email_3_title_').text();
+var _email_4_title_ = hiddenTranslation.find('._email_4_title_').text();
+var _email_5_title_ = hiddenTranslation.find('._email_5_title_').text();
+
+var _email_1_body_ = hiddenTranslation.find('._email_1_body_').text();
+var _email_2_body_ = hiddenTranslation.find('._email_2_body_').text();
+var _email_3_body_ = hiddenTranslation.find('._email_3_body_').text();
+var _email_4_body_ = hiddenTranslation.find('._email_4_body_').text();
+var _email_5_body_ = hiddenTranslation.find('._email_5_body_').text();
+
+var _random_reply_ = hiddenTranslation.find('._random_reply_').text();
+
+var _write_ = hiddenTranslation.find('._write_').text();
+var _no_subject_ = hiddenTranslation.find('._no_subject_').text();
+var _recipient_ = hiddenTranslation.find('._recipient_').text();
+var _from_ = hiddenTranslation.find('._from_').text();
+
+var _you_ = hiddenTranslation.find('._you_').text();
+var _on_ = hiddenTranslation.find('._on_').text();
+var _wrote_ = hiddenTranslation.find('._wrote_').text();
+var _original_message_ = hiddenTranslation.find('._original_message_').text();
+var _fwd_ = hiddenTranslation.find('._fwd_').text();
+
+/*
+ * Folder System
+ */
+
+var _home_folder_ = hiddenTranslation.find('._home_folder_').text();
+var _desktop_folder_ = hiddenTranslation.find('._desktop_folder_').text();
+var _pictures_folder_ = hiddenTranslation.find('._pictures_folder_').text();
+var _videos_folder_ = hiddenTranslation.find('._videos_folder_').text();
+var _music_folder_ = hiddenTranslation.find('._music_folder_').text();
+var _downloads_folder_ = hiddenTranslation.find('._downloads_folder_').text();
+var _documents_folder_ = hiddenTranslation.find('._documents_folder_').text();
+var _canonical_folder_ = hiddenTranslation.find('._canonical_folder_').text();
+var _backup_folder_ = hiddenTranslation.find('._backup_folder_').text();
+var _local_folder_ = hiddenTranslation.find('._local_folder_').text();
+var _work_folder_ = hiddenTranslation.find('._work_folder_').text();
+var _branches_folder_ = hiddenTranslation.find('._branches_folder_').text();
+var _rubbish_bin_folder_ = hiddenTranslation.find('._rubbish_bin_folder_').text();
+
+/*
+ * Software Center System
+ */
+
+var _remove_ = hiddenTranslation.find('._remove_').text();
+var _price_free_ = hiddenTranslation.find('._price_free_').text();
+var _installed_ = hiddenTranslation.find('._installed_').text();
+var _install_ = hiddenTranslation.find('._install_').text();
+var _installing_ = hiddenTranslation.find('._installing_').text();
+var _chromium_app_ = hiddenTranslation.find('._chromium_app_').text();
+var _chromium_sub_ = hiddenTranslation.find('._chromium_sub_').text();
+var _chromium_desc_ = hiddenTranslation.find('._chromium_desc_').text();
+var _beep_app_ = hiddenTranslation.find('._beep_app_').text();
+var _beep_sub_ = hiddenTranslation.find('._beep_sub_').text();
+var _beep_desc_ = hiddenTranslation.find('._beep_desc_').text();
+var _inkscape_app_ = hiddenTranslation.find('._inkscape_app_').text();
+var _inkscape_sub_ = hiddenTranslation.find('._inkscape_sub_').text();
+var _inkscape_desc_ = hiddenTranslation.find('._inkscape_desc_').text();
+var _world_of_goo_app_ = hiddenTranslation.find('._world_of_goo_app_').text();
+var _world_of_goo_sub_ = hiddenTranslation.find('._world_of_goo_sub_').text();
+var _world_of_goo_desc_ = hiddenTranslation.find('._world_of_goo_desc_').text();
+var _blender_app_ = hiddenTranslation.find('._blender_app_').text();
+var _blender_sub_ = hiddenTranslation.find('._blender_sub_').text();
+var _blender_desc_ = hiddenTranslation.find('._blender_desc_').text();
+var _braid_app_ = hiddenTranslation.find('._braid_app_').text();
+var _braid_sub_ = hiddenTranslation.find('._braid_sub_').text();
+var _braid_desc_ = hiddenTranslation.find('._braid_desc_').text();
+var _banshee_app_ = hiddenTranslation.find('._banshee_app_').text();
+var _brasero_app_ = hiddenTranslation.find('._brasero_app_').text();
+var _movie_player_app_ = hiddenTranslation.find('._movie_player_app_').text();
+var _shotwell_app_ = hiddenTranslation.find('._shotwell_app_').text();
+var _pitivi_app_ = hiddenTranslation.find('._pitivi_app_').text();
+var _sound_recorder_app_ = hiddenTranslation.find('._sound_recorder_app_').text();
+var _empathy_app_ = hiddenTranslation.find('._empathy_app_').text();
+var _thunderbird_app_ = hiddenTranslation.find('._thunderbird_app_').text();
+var _firefox_app_ = hiddenTranslation.find('._firefox_app_').text();
+var _gwibber_app_ = hiddenTranslation.find('._gwibber_app_').text();
+var _remote_desktop_app_ = hiddenTranslation.find('._remote_desktop_app_').text();
+var _terminal_app_ = hiddenTranslation.find('._terminal_app_').text();
+var _about_me_app_ = hiddenTranslation.find('._about_me_app_').text();
+var _additional_drivers_app_ = hiddenTranslation.find('._additional_drivers_app_').text();
+var _aisleroit_app_ = hiddenTranslation.find('._aisleroit_app_').text();
+var _appearance_app_ = hiddenTranslation.find('._appearance_app_').text();
+var _bluetooth_app_ = hiddenTranslation.find('._bluetooth_app_').text();
+
+/*
+ * Dash
+ */
+
+var _search_ = hiddenTranslation.find('._search_').text();
+
+/*
+ * Notifications
+ */
+
+var _turn_off_bluetooth_ = hiddenTranslation.find('._turn_off_bluetooth_').text();
+var _turn_on_bluetooth_ = hiddenTranslation.find('._turn_on_bluetooth_').text();
+var _visible_ = hiddenTranslation.find('._visible_').text();
+var _mute_ = hiddenTranslation.find('._mute_').text();
+var _unmute_ = hiddenTranslation.find('._unmute_').text();
+var _shut_down_ = hiddenTranslation.find('._shut_down_').text();
+
+
+
