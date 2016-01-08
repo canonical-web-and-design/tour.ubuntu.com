fabbae8463aefcd7df3d132017cf0232d6d8bc1b
Barry McGee <barry.mcgee@canonical.com>
2016-01-08 16:45:50 +0000

    Compress and concatenate the JS

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,10 +4,11 @@ var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var uncss = require('gulp-uncss');
+var uglify = require('gulp-uglify');
 
-// create task
+// css optimisation
 gulp.task('css', function(){
-  gulp.src('css/**/*')
+  gulp.src('css/**/*.css')
     .pipe(concat('style.css'))
     .pipe(minifyCSS())
     .pipe(uncss({
@@ -17,4 +18,38 @@ gulp.task('css', function(){
     .pipe(gulp.dest('css'))
 });
 
-gulp.task('default', ['css']);
+// js optimisation
+gulp.task('js', function(){
+  return gulp.src([
+      '!js/jquery-ui.min.js',
+      '!js/jquery.min.js',
+      'js/lang.js',
+      'js/scripts.js',
+      'js/welcomesystem.js',
+      'js/guidedtoursystem.js',
+      'js/filesystem.js',
+      'js/folders.js',
+      'js/systemoverlay.js',
+      'js/systemmenu.js',
+      'js/internetsystem.js',
+      'js/emailsystem.js',
+      'js/systemsettings.js',
+      'js/email.js',
+      'js/errormessage.js',
+      'js/ubuntuonesystem.js',
+      'js/file.js',
+      'js/shotwellsystem.js',
+      'js/shutdownSystem.js',
+      'js/movieplayersystem.js',
+      'js/libresystem.js',
+      'js/softwaresystem.js',
+      'js/notificationsystem.js',
+      'js/analytics.js'
+    ])
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(rename('script.min.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('default', ['css', 'js']);
